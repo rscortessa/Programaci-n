bc44d6eab8888d247c89bf30abca4c101c59fbe0
Ronald <rscortessa@unal.edu.co>
2018-03-01 18:15:59 -0500

    Primeros ejercicios

---
+++ Tarea1/hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main ()
+  
+{
+  std::cout << "¿Cúal es tu nombre?\n";
+  std::string Nombre, Nombre2;
+  std::cin >> Nombre >> Nombre2;
+  std::cout << "Hola, " << Nombre <<" "<< Nombre2  <<" gusto en conocerte." <<std::endl;
+  return 0;
+}
+ 
---
+++ Tarea1/mult.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <numeric>
+int main()
+{
+  int w=0;
+  std::cout <<"Ingresa un número";
+  std::cin >> w;
+  std::cout <<"Tu numero multiplicado por 2 es:" << 2*w <<std::endl;
+  return 0;
+}
+    
