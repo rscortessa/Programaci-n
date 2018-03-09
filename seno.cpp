33de17580f76b26ac81152611cfd39eb87a81c59
rscortessa <36458343+rscortessa@users.noreply.github.com>
2018-03-09 08:48:47 -0500

    Add files via upload

--- seno.cpp
+++ seno.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 #include<cmath>
+#include<cstdio>
+double f(double x)
+{
+  return std::sin(x);
+}
+double fdev(double x, double h)
+{
+  double y=(f(x+h)-f(x))/h;
+  return y;
+}
 int main (void)
 {
-  double x = M_PI/2.0;
-    std::cout << "sin("<<x<<") = " << std:: sin(x) << std::endl;
+  double x=M_PI/3;
+  double h=0.1;
+  std::printf("%25.16e\n",fdev(x,h));
+  return 0;
 }
+  
+    
+
+	    
